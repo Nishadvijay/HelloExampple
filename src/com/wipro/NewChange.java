7e4bb3346b8c118ada60e944828ef518e6d2865f
Nishadvijay <42165216+Nishadvijay@users.noreply.github.com>
2019-12-16 19:49:11 +0530

    new method added tclass
    using checkout functioality

--- src/com/wipro/NewChange.java
+++ src/com/wipro/NewChange.java
@@ -1,7 +1,10 @@
 package com.wipro;
 
 public class NewChange {
-
+		
+	public void newMethod(){
+	System.out.println("new  changes");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("new request changes");
